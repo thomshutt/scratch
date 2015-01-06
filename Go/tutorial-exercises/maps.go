c1c02a4ea7e18b6b2e2aa17a719dc0d6b6aa5e4f
Thom Shutt <thom@thomshutt.com>
2015-01-06 23:35:49 +0000

    Did maps exercise

---
+++ Go/tutorial-exercises/maps.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"code.google.com/p/go-tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		m[v] += 1
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
